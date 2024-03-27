616463da1466e926258dff52103cb2d23e058455
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-03-27 19:03:31 +0600

    Testimonail functionality done

--- app/Http/Controllers/Admin/Testimonal.php
+++ app/Http/Controllers/Admin/Testimonal.php
@@ -60,19 +60,95 @@ class Testimonal extends Controller
     //edit Testimonal
     public function edit($id)
     {
+        try {
+
+           $id=decrypt($id);
+           $item=Testimonial::findOrFail($id);
+           return view('admin.testimonial.edit',compact($item));
+        }
+        catch(\Exception $e)
+        {
+            return redirect()->back()->withErrors($e->getMessage());
+        }
     }
 
     public function update(Request $request, $id)
     {
+          try{
+            $validate = Validator::make($request->all(), [
+                "client_name" => ['required', 'string', 'max:255', 'min:3'],
+                "image" => ['nullable', 'image', 'mimes:png,jpg', 'max:3096'],
+                "quote" => ["rquired", 'string', 'max:255', 'min:3'],
+
+            ]);
+
+            if ($validate->fails()) {
+                return redirect()->back()->withErrors($validate->errors())->withInput();
+            }
+
+            $id=decrypt($id);
+            $item=Testimonial::findOrFail($id);
+
+            //check Image
+            $image_url=null;
+            if($request->file('image'))
+            {
+                //Remove Previous image
+
+                if($item->image!==null)
+                {
+                    unlink(storage_path($item->image));
+                }
+
+                $image = $request->file('image');
+            $image_name = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            $path = 'images/testimonal/';
+            if (!File::exists(storage_path($path))) {
+                File::makeDirectory(storage_path($path), 0777, true);
+            }
+            Image::make($image)->resize(600, 600)->save(storage_path('images/testimonal/' . $image_name));
+            $image_url = 'images/testimonal/' . $image_name;
+            }
+
+            $item->client_name=$request->client_name;
+            $item->image=$image_url==null?$item->image:$image_url;
+            $item->quote=$request->quote;
+
+            if($item->isClean())
+            {
+                return redirect()->back()->with(['toast-type'=>'info','toast-message'=>'Nothing to update!']);
+            }
+
+            $item->save();
+
+            return redirect()->back()->with(['toast-type'=>'success','toast-message'=>'Testimonial has been updated!']);
+          }catch(\Exception $e)
+
+          {
+            return redirect()->back()->withErrors($e->getMessage());
+          }
     }
 
     //Show All Testimonals
     public function index()
     {
+        $items=Testimonial::get();
+        return view('admin.testimonial.index',compact('items'));
     }
 
     //Active Inactve testimonials
     public function statusChange($id)
     {
+        try{
+
+            $id=decrypt($id);
+            $item=Testimonial::findOrFail($id);
+            $item->status=$item->status=='1'?'0':'1';
+            $item->save();
+            return redirect()->back()->with(['toast-type'=>'success','toast-message'=>"Successfully Testimonial status changed to ".$item->status=='1'?'Active':'inactive']);
+        }
+        catch(\Exception $e){
+            return redirect()->back()->withErrors($e->getMessage());
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Testimonal;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,9 +12,17 @@ Route::get('/dashboard', function () {
     return view('admin.home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::controller(Testimonal::class)->group(function () {
+        Route::get('add-testimonial','add')->name('admin.testimonial.add');
+        Route::post('add-testimonial','store');
+        Route::get('edit-testimonial/{id}','edit')->name('admin.testimonial.edit');
+        Route::put('edit-testimonial/{id}','update')->name('admin.testimonial.edit');
+        Route::put('testimonal-status-change/{id}','statusChange')->name('admin.testimonial.status');
+
+    });
 
 });
 
