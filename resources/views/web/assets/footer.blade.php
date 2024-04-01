de4216c12b26776c574cce1d6620f777015525f6
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-04-01 16:40:34 +0600

    Front end Footer Dynamic done

--- resources/views/web/assets/footer.blade.php
+++ resources/views/web/assets/footer.blade.php
@@ -1,4 +1,8 @@
  <!-- Footer -->
+ @php
+     $item=App\Models\Contact::latest()->first();
+
+ @endphp
  <footer class="bg-black">
     <div class="container mx-auto font-montserrat py-16 px-5 md:px-0 md:mt-0">
         <div class="flex flex-col justify-center items-center mx-auto max-w-[39rem] ">
@@ -11,9 +15,9 @@
             <div class="max-w-[14.63rem] ">
                 <h2 class="font-montserrat font-medium text-xl text-primary">Office</h2>
                 <div class="pt-5 font-sans text-neutral space-y-3">
-                    <p class="">House: 62, Dour Main Road, Block: D, Uttara Rajabari, Turag, Dhaka</p>
-                    <p class="">hello[@]webpicodigital.com</p>
-                    <p class="">+88 01752 598101</p>
+                    <p class="">{{ $item?->address??'House: 62, Dour Main Road, Block: D, Uttara Rajabari, Turag, Dhaka' }}</p>
+                    <p class="">{{ $item?->email??'hello[@]webpicodigital.com' }}</p>
+                    <p class="">{{ $item->mobile??'+88 01752 598101' }}</p>
                 </div>
             </div>
             <div class="max-w-[14.63rem]">
@@ -34,7 +38,7 @@
                     <li class=""><a href="" class="">Dribble</a></li>
                 </ul>
             </div>
-            <img onclick="scrollToTop()" class="h-9 w-9 absolute right-3 -bottom-3 cursor-pointer" src="./assets/scroll-top.svg" alt="">
+            <img onclick="scrollToTop()" class="h-9 w-9 absolute right-3 -bottom-3 cursor-pointer" src="{{ asset('src/assets/scroll-top.svg') }}" alt="">
         </div>
         <p class="text-neutral font-montserrat max-w-[55rem] mx-auto">Webpico Digital © {{ date('Y') }} All Rights Reserved. | Developved By <a href="https://abovebd.com/">Above IT</a></p> This website is under development
     </div>
