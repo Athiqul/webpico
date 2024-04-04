0a849af84eb058a99ac1d3a557a4fb4e4e9f7442
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-04-04 14:17:53 +0600

    Services category List view API done

--- app/Http/Controllers/Admin/ServicesCategoryController.php
+++ app/Http/Controllers/Admin/ServicesCategoryController.php
@@ -3,9 +3,104 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ServicesCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ServicesCategoryController extends Controller
 {
-    //
+
+
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            "name" => ['required', 'string', 'max:255', 'min:3','unique:social_media,name'],
+            "link" => ['required','max:255'],
+
+
+        ]);
+
+        if ($validate->failed()) {
+
+        }
+
+
+
+
+
+    }
+
+    //edit Testimonal
+    public function edit($id)
+    {
+        try {
+
+           $id=decrypt($id);
+           $item=ServicesCategory::findOrFail($id);
+
+        }
+        catch(\Exception $e)
+        {
+            dd($e->getMessage());
+
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+          try{
+            $validate = Validator::make($request->all(), [
+                "name" => ['required', 'string', 'max:255', 'min:3'],
+            "link" => ['required','max:255'],
+
+            ]);
+
+            if ($validate->failed()) {
+                return redirect()->back()->withErrors($validate->errors())->withInput();
+            }
+
+            $id=decrypt($id);
+            $item=ServicesCategory::findOrFail($id);
+
+           //Check name is uniqu or not
+           $checkName=ServicesCategory::where('name','=',$item->name)->where('id','!=',$id)->first();
+           if($checkName)
+           {
+               return redirect()->back()->with(['toast-type'=>'warning','toast-message'=>'Name Already Exist']);
+           }
+
+
+            $item->name=$request->name;
+            $item->link=$request->link;
+
+
+            if($item->isClean())
+            {
+                return redirect()->back()->with(['toast-type'=>'info','toast-message'=>'Nothing to update!']);
+            }
+
+            $item->save();
+
+            return redirect()->back()->with(['toast-type'=>'success','toast-message'=>'Socail Media has been updated!']);
+          }catch(\Exception $e)
+
+          {
+            dd($e->getMessage());
+            return redirect()->back()->with(['toast-type'=>'danger','toast-message'=>'Something went wrong!']);
+          }
+    }
+
+
+    //Show All Testimonals
+    public function index()
+    {
+        $items=ServicesCategory::get();
+        return response()->json(
+            $items,
+            200
+        );
+    }
+
+
+
 }
---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Admin\ServicesCategoryController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('services-categories',[ServicesCategoryController::class,'index'])->name('services_categories.api');
