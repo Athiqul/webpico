8dc5c90fddda786a0620f0baa40170933bf1da3a
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-04-06 17:47:01 +0600

    Services Navbar done

--- app/Models/ServicesCategory.php
+++ app/Models/ServicesCategory.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ServicesCategory extends Model
 {
     protected $guarded=[];
     use HasFactory;
+
+    //get sub categories
+    public function subCategories(): HasMany
+    {
+        return $this->hasMany(SubCategory::class,'category_id');
+    }
 }
--- resources/views/web/assets/navbar.blade.php
+++ resources/views/web/assets/navbar.blade.php
@@ -13,67 +13,52 @@
                         <a href="" class="">Services</a>
                     </li>
                     <div class="hidden group-hover:block absolute">
-                        <li onclick="seoToggle()"
+                        @php
+                            $categories=\App\Models\ServicesCategory::with('subcategories')->get();
+                        @endphp
+
+                         @foreach ($categories as $category)
+
+
+                         @if (count($category->subcategories)>0)
+                         <li onclick="toggleHandle({{$category->id}})"
                             class="dropdown-link flex gap-6 justify-between items-center relative group">
-                            <p class="">SEO</p>
-                            <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_200_1415)">
-                                    <path
-                                        d="M3.04872 0.133138L7.59417 4.67871C7.67942 4.76392 7.72729 4.87956 7.72729 5.00011C7.72729 5.12068 7.67939 5.23629 7.59414 5.32153L3.04869 9.86686C2.87117 10.0444 2.58339 10.0444 2.40587 9.86686C2.22839 9.68938 2.22836 9.40156 2.40587 9.22405L6.6299 5.00011L2.40587 0.775955C2.31711 0.687198 2.27275 0.570865 2.27275 0.454532C2.27275 0.338199 2.31711 0.221865 2.4059 0.133107C2.58342 -0.0443774 2.8712 -0.0443774 3.04872 0.133138Z"
-                                        fill="white" />
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_200_1415">
-                                        <rect width="10" height="10" fill="white"
-                                            transform="matrix(1.19249e-08 -1 -1 -1.19249e-08 10 10)" />
-                                    </clipPath>
-                                </defs>
-                            </svg>
-                            <ul id="seoMenu" class="hidden dropdown-list absolute -right-[8.2rem] top-0">
-                                <li class="dropdown-link flex gap-6 justify-between items-center">
-                                    <a href="" class="">Local SEO</a>
-                                </li>
-                                <li class="dropdown-link flex gap-6 justify-between items-center">
-                                    <a href="" class="">E-Commerce</a>
-                                </li>
-                                <li class="dropdown-link flex gap-6 justify-between items-center">
-                                    <a href="" class="">Multi Location</a>
-                                </li>
-                            </ul>
+                            <p class="">{{ ucwords($category->name) }}</p>
+                         <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                         <g clip-path="url(#clip0_200_1415)">
+                             <path
+                                 d="M3.04872 0.133138L7.59417 4.67871C7.67942 4.76392 7.72729 4.87956 7.72729 5.00011C7.72729 5.12068 7.67939 5.23629 7.59414 5.32153L3.04869 9.86686C2.87117 10.0444 2.58339 10.0444 2.40587 9.86686C2.22839 9.68938 2.22836 9.40156 2.40587 9.22405L6.6299 5.00011L2.40587 0.775955C2.31711 0.687198 2.27275 0.570865 2.27275 0.454532C2.27275 0.338199 2.31711 0.221865 2.4059 0.133107C2.58342 -0.0443774 2.8712 -0.0443774 3.04872 0.133138Z"
+                                 fill="white" />
+                         </g>
+                         <defs>
+                             <clipPath id="clip0_200_1415">
+                                 <rect width="10" height="10" fill="white"
+                                     transform="matrix(1.19249e-08 -1 -1 -1.19249e-08 10 10)" />
+                             </clipPath>
+                         </defs>
+                     </svg>
+                         <ul id="menu-{{ $category->id }}" class="findUi hidden  dropdown-list absolute -right-[8.2rem] top-0">
+                             @foreach ($category->subcategories as $subcategory)
+                             <li class="dropdown-link flex gap-6 justify-between items-center">
+                                <a href="" class="">{{ ucwords($subcategory->name) }}</a>
+                            </li>
+                             @endforeach
+
+
+                        </ul>
+
+
+                     </li>
+                        @else
+                        <li class="dropdown-link " onclick="clearMenu()">
+                            <a  class="whitespace-nowrap">{{ ucwords($category->name) }}</a>
                         </li>
-                        <li onclick="marketingToggle()"
-                            class="dropdown-link flex gap-6 justify-between items-center relative group">
-                            <p class="whitespace-nowrap">Digital Marketing</p>
-                            <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <g clip-path="url(#clip0_200_1415)">
-                                    <path
-                                        d="M3.04872 0.133138L7.59417 4.67871C7.67942 4.76392 7.72729 4.87956 7.72729 5.00011C7.72729 5.12068 7.67939 5.23629 7.59414 5.32153L3.04869 9.86686C2.87117 10.0444 2.58339 10.0444 2.40587 9.86686C2.22839 9.68938 2.22836 9.40156 2.40587 9.22405L6.6299 5.00011L2.40587 0.775955C2.31711 0.687198 2.27275 0.570865 2.27275 0.454532C2.27275 0.338199 2.31711 0.221865 2.4059 0.133107C2.58342 -0.0443774 2.8712 -0.0443774 3.04872 0.133138Z"
-                                        fill="white" />
-                                </g>
-                                <defs>
-                                    <clipPath id="clip0_200_1415">
-                                        <rect width="10" height="10" fill="white"
-                                            transform="matrix(1.19249e-08 -1 -1 -1.19249e-08 10 10)" />
-                                    </clipPath>
-                                </defs>
-                            </svg>
-                            <ul id="marketingMenu" class="hidden dropdown-list absolute -right-[10.4rem] top-0">
-                                <li class="dropdown-link flex gap-6 justify-between items-center">
-                                    <a href="" class="">SMM</a>
-                                </li>
-                                <li class="dropdown-link flex gap-6 justify-between items-center">
-                                    <a href="" class="">Google Ads</a>
-                                </li>
-                                <li class="dropdown-link flex gap-6 justify-between items-center">
-                                    <a href="" class="">Content Marketing</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="dropdown-link ">
-                            <a href="" class="whitespace-nowrap">Video Editing</a>
-                        </li>
+                        @endif
+                         @endforeach
+
+
+
                     </div>
                 </div>
                 <li class="nav-link">
@@ -154,4 +139,40 @@
         </li>
     </ul>
 </nav>
+<script>
+    function toggleHandle(id) {
+    let ul = document.getElementById("menu-"+id);
+    let allUi=document.querySelectorAll('.findUi');
+    allUi.forEach(function(item){
+        item.classList.add('hidden');
+    });
+    //console.log(ul);
+
+   // console.log(classList);
+    if (ul.classList.contains('hidden')) {
+        ul.classList.remove('hidden');
+        return
+    } else {
+        ul.classList.add('hidden');
+        return
+    }
+
+}
+
+function clearMenu()
+{
+
+    let allUi=document.querySelectorAll('.findUi');
+    allUi.forEach(function(item){
+       if(item.classList.contains('hidden'))
+       {
+
+
+       }else{
+            item.classList.add('hidden');
+       }
+
+    });
+}
+</script>
 <!-- Navbar End -->
