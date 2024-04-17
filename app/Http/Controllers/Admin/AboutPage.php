c14651bf162e3a2dc0935b44e6520f8ffd6bb635
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-04-17 14:47:01 +0600

    About Page Controller Logic done

--- app/Http/Controllers/Admin/AboutPage.php
+++ app/Http/Controllers/Admin/AboutPage.php
@@ -3,9 +3,154 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\About;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 
 class AboutPage extends Controller
 {
-    //
+    public function edit()
+    {
+        try{
+
+            // Find the OurWork blog entry by ID
+
+            $item = About::latest()->first();
+
+            // Return a view for editing with the retrieved data
+            return view('admin.about.edit', compact('item'));
+        }catch(\Exception $e){
+            return redirect()->back()->with(['toast-type'=>'error','toast-message'=>'Something went wrong!']);
+        }
+
+
+    }
+
+    public function update(Request $request)
+    {
+        try{
+
+            $validatedData = $request->validate([
+                'about_title' => 'required|string|max:255|min:3',
+                'about_short_desc' => 'required|min:3',
+                'about_left_desc' => 'required|min:3',
+                'about_right_desc' => 'required|min:3',
+                'left_image' => 'required|image|mimes:png,jpg|max:2048',
+                'right_image' => 'required|image|mimes:png,jpg|max:2048',
+                'video_link'=>'required|string|max:255',
+
+            ]);
+
+            // Find the About latest row
+            $about = About::latest()->first();
+
+
+            $left_image=null;
+            $right_image=null;
+            if($request->hasFile('left_image'))
+            {
+                 //check existence of images if about exist
+                 if($about)
+                 {
+                    $path = storage_path('app/uploads/about/' . $about->left_image);
+                    if(File::exists($path))
+                    {
+                        try {
+                            // Delete the file
+                            File::delete($path);
+                        } catch (\Exception $e) {
+
+                        }
+                    }
+                 }
+
+
+                //Save the image
+                $image = $request->file('left_image');
+                $left_image = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+                $path = $image->storeAs('about', $left_image, 'uploads');
+                // Full path to the saved image
+                $fullPath = storage_path("app/uploads/{$path}");
+
+                Image::make($fullPath)->fit(1100, 880, function ($constraint) {
+                    $constraint->upsize();
+                });
+            }
+            if($request->hasFile('right_image'))
+            {
+                 //check existence of images if about exist
+                 if($about)
+                 {
+                    $path = storage_path('app/uploads/about/' . $about->right_image);
+                    if(File::exists($path))
+                    {
+                        try {
+                            // Delete the file
+                            File::delete($path);
+                        } catch (\Exception $e) {
+
+                        }
+                    }
+                 }
+
+
+                //Save the image
+                $image = $request->file('right_image');
+                $right_image = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+                $path = $image->storeAs('about', $right_image, 'uploads');
+                // Full path to the saved image
+                $fullPath = storage_path("app/uploads/{$path}");
+
+                Image::make($fullPath)->fit(1100, 880, function ($constraint) {
+                    $constraint->upsize();
+                });
+            }
+
+            //About is not added yet
+            if(!$about)
+            {
+               $data=[
+                'about_title'=>$validatedData['about_title'],
+                'about_short_desc'=>$validatedData['about_short_desc'],
+                'about_left_desc'=>$validatedData['about_left_desc'],
+                'about_right_desc'=>$validatedData['about_right_desc'],
+                'left_image'=>$left_image,
+                'right_image'=>$right_image,
+                'video_link'=>$validatedData['video_link'],
+               ];
+
+               About::create($data);
+               return redirect()->back()->with(['toast-type'=>'success','toast-message'=>'About page updated successfully!']);
+            }
+
+            $about->about_title = $validatedData['about_title'];
+            $about->about_short_desc = $validatedData['about_short_desc'];
+            $about->about_left_desc = $validatedData['about_left_desc'];
+            $about->about_right_desc = $validatedData['about_right_desc'];
+            $about->left_image=$left_image??$about->left_image;
+            $about->right_image=$right_image??$about->right_image;
+            $about->video_link=$validatedData['video_link'];
+
+
+
+            if($about->isClean())
+            {
+                return redirect()->back()->with(['toast-type'=>'info','toast-message'=>'Nothing to update!']);
+            }
+
+
+
+            // Save the updated OurWork blog entry
+            $about->save();
+
+            // Redirect to a route after successful update
+            return redirect()->back()->with(['toast-type'=>'success','toast-message'=>'About Page updated successfully!']);
+        }catch(\Exception $e){
+            dd($e->getMessage());
+            return redirect()->back()->with(['toast-type'=>'error','toast-message'=>'Something went wrong!']);
+        }
+        // Validate the incoming request data
+
+    }
 }
---
+++ resources/views/admin/about/edit.blade.php
@@ -0,0 +1,214 @@
+@extends('admin.layout.master_layout')
+
+@section('title')
+    {{ $item->title }}| MasterSeller
+@endsection
+
+@section('need-css')
+    <link href="{{ asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/select2/css/select2-bootstrap4.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('assets/plugins/notifications/css/lobibox.min.css') }}">
+    <link href="{{ asset('assets/plugins/input-tags/css/tagsinput.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.12/dist/sweetalert2.min.css">
+    <script src="https://cdn.tiny.cloud/1/b69tdpiu66ovx82jjhzsf0eooi7hehgia7avmhbdiy1s6rx4/tinymce/6/tinymce.min.js"
+        referrerpolicy="origin"></script>
+
+
+@endsection
+
+
+@section('main')
+    <div class="page-content" data-select2-id="27">
+        <!--breadcrumb-->
+        <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+            <div class="breadcrumb-title pe-3">Edit Blog</div>
+            <div class="ps-3">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb mb-0 p-0">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.blogs.index') }}"><i
+                                    class="bx bx-home-alt"></i></a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">Update Blog</li>
+                    </ol>
+                </nav>
+            </div>
+
+        </div>
+        <!--end breadcrumb-->
+        <div class="row" data-select2-id="26">
+            <div class="col-xl-9 mx-auto" data-select2-id="25">
+                <h6 class="mb-0 text-uppercase">Edit Blog</h6>
+                <hr>
+                <div class="card" data-select2-id="24">
+                    <div class="card-body" data-select2-id="23">
+                        <form method="POST" action="{{ route('admin.blogs.edit',$item->id) }}" id="transfer" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="border p-3 rounded" data-select2-id="22">
+
+
+                                <div class="col-12 mb-2">
+                                    <label for="amount" class="form-label">Type Title</label>
+                                    <div class=" form-group ">
+                                      <input type="text" class="form-control border-start-0" name="title" placeholder="Type title of blog" title="Type title" value="{{ old('title',$item->title) }}" required>
+
+                                    </div>
+
+                                    @error('title')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+
+                                </div>
+
+
+                                <div class="col-12 mb-2">
+                                    <label for="amount" class="form-label">Description</label>
+                                    <div class="input-group form-group">
+                                        <textarea name="desc" id="" rows="10" required>{!! old('desc',$item->desc) !!} </textarea>
+
+                                    </div>
+                                    @error('desc')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+
+                                </div>
+
+                                <div class="col-12 mb-2">
+                                    <label for="amount" class="form-label">SEO Meta Description</label>
+                                    <div class="input-group form-group">
+                                        <input type="text" class="form-control border-start-0" name="meta_desc" placeholder="Type meta description of this blog" title="Type meta descriptions" value="{{ old('meta_desc',$item->meta_desc??'') }}">
+                                    </div>
+                                    @error('meta_desc')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label">Tags</label>
+                                    <div class="input-group form-group">
+                                        <input type="text" name="tags" class="form-control visually-hidden" data-role="tagsinput" value="{{ old('tags',$item->tags) }}" required>
+                                    </div>
+
+                                    @error('tags')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                                </div>
+
+                                <div class="mb-3">
+                                    <label class="form-label">Meta Keywords</label>
+                                    <div class="input-group form-group">
+                                        <input type="text" name="meta_keywords" class="form-control visually-hidden" data-role="tagsinput" value="{{ old('meta_keywords',$item->meta_keywords??'') }}" >
+                                    </div>
+
+                                    @error('meta_keywords')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                                </div>
+
+                                <div class="col-12 mb-2">
+                                    <label for="amount" class="form-label">Upload Image </label>
+                                    <div class=" form-group ">
+                                      <input type="file" class="form-control border-start-0" name="image" accept="image/jpeg,image/png" onchange="changeImage(event)"  title="upload image" value="{{ old('image') }}" >
+
+                                    </div>
+
+                                    @error('image')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+
+                                </div>
+
+                                <div class="col-6 mb-2">
+                                    <img src="{{ route('public.image',['blogs',$item->image]) }}" id="preview" width="300px" height="250px" alt="">
+
+                                </div>
+
+
+
+
+
+
+
+                                <div class="col-12 mt-3">
+                                    <button type="submit" class="btn btn-info text-light px-5"> <i
+                                            class="bx bxs-send"></i>Update Blog</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end row-->
+    </div>
+@endsection
+
+
+@section('need-js')
+    <script src="{{ asset('assets/plugins/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('assets/js/validate.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/notifications/js/lobibox.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/notifications/js/notifications.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/input-tags/js/tagsinput.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script>
+
+
+      function changeImage(event)
+{
+  if(event.target.files.length>0){
+    var src=URL.createObjectURL( event.target.files[0]);
+    let preview=document.getElementById('preview');
+    preview.src=src;
+  }
+}
+        tinymce.init({
+            selector: 'textarea',
+            plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker',
+            toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
+        });
+
+
+
+        $('#transfer').validate({
+            rules: {
+               title: {
+                    required: true,
+
+
+                },
+
+                desc: {
+                    required: true,
+
+
+
+                },
+
+                tags:{
+                  required: true,
+                }
+
+
+
+            },
+
+
+            errorElement: 'span',
+            errorPlacement: function(error, element) {
+                error.addClass('invalid-feedback');
+                element.closest('.form-group').append(error);
+            },
+            highlight: function(element, errorClass, validClass) {
+                $(element).addClass('is-invalid');
+            },
+            unhighlight: function(element, errorClass, validClass) {
+                $(element).removeClass('is-invalid');
+            },
+        });
+    </script>
+@endsection
