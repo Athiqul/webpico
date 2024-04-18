6fd92eeb851bec2409907ad5d95267f32ba5322b
Athiqulhasan <athiqulhasan.4@gmail.com>
2024-04-18 15:06:52 +0600

    Task done

--- resources/views/web/assets/latest.blade.php
+++ resources/views/web/assets/latest.blade.php
@@ -1,39 +1,49 @@
 <!-- New Article -->
+@php
+    $latest =App\Models\Blogs::where('status','1')->latest()->first();
+@endphp
+
+@if ($latest)
 <section id="" class="container mx-auto py-10">
     <div class="pb-14">
         <h3 class="title">NEW ARTICLES</h3>
         <p class="description">Check our latest blog posts</p>
     </div>
     <div class="relative mb-10">
-        <img class="max-h-[37rem] hidden md:block w-screen px-5 md:px-0" src="{{ asset('src/assets/article.jpeg') }}" alt="">
+        <img class="max-h-[37rem] hidden md:block w-screen px-5 md:px-0" src="{{ route('public.image',['blogs', $latest->image]) }}" alt="{{ $latest->title }}">
         <div class="bg-white block md:absolute px-10 pt-16 pb-8 top-0 left-5 md:left-20 max-w-[29.38rem] ">
             <p class="font-montserrat text-base md:text-xl text-neutral font-medium">FEATURED ARTICLE</p>
-            <p class="font-montserrat font-bold text-xl md:text-2xl text-primary">The Evolution of Web Development:
+            <p class="font-montserrat font-bold text-xl md:text-2xl text-primary">{{ $latest->title }}:
             </p>
             <div class="flex items-center gap-4 mt-5 mb-3">
-                <p class="text-secondary">Web Development</p>
+                <p class="text-secondary">  @php
+                    $tag=explode(',',$latest->tags);
+                @endphp {{ ucwords($tag[0]) }}</p>
                 <div class="h-2 w-2 rounded-full bg-neutral"></div>
-                <p class="text-neutral">May 7, 2021 (10 mins read)</p>
+                @php
+                $perminuteWords=200;
+                $words=str_word_count(strip_tags($latest->desc));
+                $minutes=ceil($words/$perminuteWords);
+
+            @endphp
+                <p class="text-neutral">{{ date('M d,Y',strtotime($latest->created_at)) }} ({{ $minutes }} {{ $minutes==1?'min':'mins' }} read)</p>
             </div>
-            <p class="font-sans text-neutral mb-3">Proident aliquip velit qui commodo officia qui consectetur dolor
-                ullamco aliquip elit
-                incididunt. Ea minim ex consectetur excepteur. Ex laborum nostrud mollit sint consectetur Lorem amet
-                aliqua do enim. Commodo duis dolor anim excepteur. In aliquip mollit nulla consequat velit magna.
-            </p>
+            @php
+                $words=explode(' ',strip_tags($latest->desc));
+                $words=array_slice($words,0,20);
+                $words=implode(' ',$words);
+                $words.='...';
+            @endphp
+            {{ $words }}
             <div class="flex items-center justify-between">
-                <div class="flex gap-4">
-                    <button type="button" class="rounded bg-neutral px-2 py-3">
-                        <img src="./assets/arrow-left.svg" alt="" class="">
-                    </button>
-                    <button type="button" class="rounded bg-primary px-2 py-3">
-                        <img src="./assets/arrow-right2.svg" alt="" class="">
-                    </button>
-                </div>
-                <button class="bg-primary rounded px-3 py-2 text-white text-sm md:text-base" type="button">
+
+                <a href="{{ route('web.blog_details',encrypt($latest->id)) }}" class="bg-primary rounded px-3 py-2 text-white text-sm md:text-base" type="button">
                     Continue Reading
-                </button>
+                </a>
             </div>
         </div>
     </div>
 </section>
+@endif
+
 <!-- New Article End -->
